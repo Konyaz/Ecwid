f0b83cb5511bcb90a42614e7e721a06ba0cc8de7
Konyaz <80565760+Konyaz@users.noreply.github.com>
2021-06-11 01:03:39 +0900

    add

--- src/test/java/tests/ShoppingTestsUi.java
+++ src/test/java/tests/ShoppingTestsUi.java
@@ -13,30 +13,30 @@ public class ShoppingTestsUi extends TestBase {
     @Test
     void addAndBuyUITest() {
         step("Open site", () -> {
-        open("https://buy-in-10-seconds.company.site/");
+            open("https://buy-in-10-seconds.company.site/");
         });
         step("Shopping Cart", () -> {
-        $(".grid-product__shadow-inner").click();
-        $x("//button[contains(.,'В корзину')]").click();
-        $x("//button[contains(.,'Корзина')]").click();
+            $(".grid-product__shadow-inner").click();
+            $x("//button[contains(.,'В корзину')]").click();
+            $x("//button[contains(.,'Корзина')]").click();
         });
         step("Registration form", () -> {
-        $("#ec-cart-email-input").sendKeys("two@1.ru");
-        $("#form-control__checkbox--agree").click();
-        $(".form-control__loader").click();
-        $("#ec-country").click();
-        $(byText("Перу")).click();
-        $("#ec-full-name").sendKeys("Alex");
-        $("#ec-phone").sendKeys("8800");
-        $("#ec-address-line1").sendKeys("Street");
-        $("#ec-city-list").sendKeys("City");
-        $("#ec-postal-code").sendKeys("191919");
-        $(".form-control__select").click();
-        $(byText("Ica")).click();
-        $(".form-control__loader").click();
+            $("#ec-cart-email-input").sendKeys("two@1.ru");
+            $("#form-control__checkbox--agree").click();
+            $(".form-control__loader").click();
+            $("#ec-country").click();
+            $(byText("Перу")).click();
+            $("#ec-full-name").sendKeys("Alex");
+            $("#ec-phone").sendKeys("8800");
+            $("#ec-address-line1").sendKeys("Street");
+            $("#ec-city-list").sendKeys("City");
+            $("#ec-postal-code").sendKeys("191919");
+            $(".form-control__select").click();
+            $(byText("Ica")).click();
+            $(".form-control__loader").click();
         });
         step("Verify successful buy", () -> {
-        $(".ec-header-h1").shouldHave(text("Спасибо за заказ! "));
+            $(".ec-header-h1").shouldHave(text("Спасибо за заказ! "));
         });
     }
 }
