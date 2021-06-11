20655bd29c8c236553e729fa1aad4063fa622f9f
Konyaz <80565760+Konyaz@users.noreply.github.com>
2021-06-11 20:13:52 +0900

    add

--- src/test/java/tests/ShoppingTestsUi.java
+++ src/test/java/tests/ShoppingTestsUi.java
@@ -1,17 +1,20 @@
 package tests;
 
+
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
+
 
 public class ShoppingTestsUi extends TestBase {
 
 
     @Test
     void addAndBuyUITest() {
+
+
         step("Open site", () -> {
             open("https://buy-in-10-seconds.company.site/");
         });
@@ -21,14 +24,17 @@ public class ShoppingTestsUi extends TestBase {
             $x("//button[contains(.,'Корзина')]").click();
         });
         step("Registration form", () -> {
-            $("#ec-cart-email-input").sendKeys("two@1.ru");
+            $("#ec-cart-email-input").setValue("twotwo@1.ru");
             $("#form-control__checkbox--agree").click();
             $(".form-control__loader").click();
-            $("#ec-full-name").sendKeys("Alex");
-            $("#ec-address-line1").sendKeys("Street");
-            $("#ec-city-list").sendKeys("City");
-            $("#ec-postal-code").sendKeys("01067");
+            $("#ec-country").click();
+
+            $("#ec-full-name").setValue("Alex");
+            $("#ec-address-line1").setValue("Street");
+            $("#ec-city-list").setValue("City");
+            $("#ec-postal-code").setValue("01067");
             $(".form-control__loader").click();
+
         });
         step("Verify successful buy", () -> {
             $(".ec-header-h1").shouldHave(text("Спасибо за заказ! "));
